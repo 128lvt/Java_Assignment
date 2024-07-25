4e72e0eab0fa3be0d7c7ebff375a5bf2f664ea74
Admin <128vanthoai@gmail.com>
2024-07-26 01:10:44 +0700

    client update product

--- src/main/resources/templates/admin/js/script.js
+++ src/main/resources/templates/admin/js/script.js
@@ -4,18 +4,68 @@ var LIMIT_PAGE = 15;
 
 app.controller("MainController", function ($scope, $http) {
   $scope.products = [];
+  $scope.currentPage = 0;
+  $scope.totalEntries = 0;
+  $scope.totalPages = 0;
 
-  $scope.getItems = function () {
+  $scope.getItems = function (page) {
     $scope.limit = LIMIT_PAGE;
     $http
-      .get(API_PREFIX + "products?page=0" + "&limit=" + $scope.limit)
+      .get(API_PREFIX + "products?page=" + page + "&limit=" + $scope.limit)
       .then(function (response) {
         $scope.products = response.data.products;
         $scope.totalEntries = response.data.totalItems;
         $scope.totalPages = response.data.totalPages;
-        console.log($scope.products);
+        $scope.currentPage = page;
       });
   };
 
-  $scope.getItems();
+  $scope.editProduct = function (productId) {
+    $scope.selectedProduct = $scope.products.find((p) => p.id === productId);
+    $("#editProductModal").modal("show");
+  };
+
+  $scope.updateProduct = function () {
+    if ($scope.selectedProduct) {
+      $http
+        .put(
+          API_PREFIX + "products/" + $scope.selectedProduct.id,
+          $scope.selectedProduct
+        )
+        .then(function (response) {
+          $scope.getItems($scope.currentPage);
+          $("#editProductModal").modal("hide");
+        })
+        .catch(function (error) {
+          console.error("Error updating product:", error);
+        });
+    }
+  };
+
+  $scope.updateProduct = function () {
+    if ($scope.selectedProduct) {
+      var productData = {
+        name: $scope.selectedProduct.name,
+        price: $scope.selectedProduct.price,
+        description: $scope.selectedProduct.description,
+        category_id: $scope.selectedProduct.category.categoryId,
+      };
+
+      $http({
+        method: "PUT",
+        url: API_PREFIX + "products/" + $scope.selectedProduct.id,
+        data: productData,
+        headers: { "Content-Type": "application/json" },
+      })
+        .then(function (response) {
+          $scope.getItems(0); // Refresh the product list
+          $("#editProductModal").modal("hide");
+        })
+        .catch(function (error) {
+          console.error("Error updating product:", error);
+        });
+    }
+  };
+
+  $scope.getItems(0);
 });
